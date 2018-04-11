6aaacd52fee0981943abdae743ea3756dc4627d7
Benjamin <bnjmnsepulveda@gmail.com>
2018-04-11 17:16:30 -0300

    inicio repositorio

---
+++ src/main/CommandService.java
@@ -0,0 +1,111 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Servicio de ejecucion de comandos y procesos.
+ *
+ * @author Roberto
+ */
+public class CommandService {
+
+    /**
+     * Ejecuta un comando y devuelve la salida en String.
+     *
+     * @param cmd comando a ejecutar
+     * @return
+     * @throws java.io.IOException
+     */
+    public String executeCommand2(String cmd) throws IOException {
+        String linea;
+        String salida = null;
+        String lineaError;
+        String salidaError;
+        Process proceso;
+        InputStreamReader entrada = null;
+        InputStreamReader error = null;
+        BufferedReader stdInput = null;
+        BufferedReader stdError = null;
+        try {
+            String[] pipelineCmd = {
+                "/bin/bash",
+                "-c",
+                cmd
+            };
+            proceso = Runtime.getRuntime().exec(pipelineCmd);
+            entrada = new InputStreamReader(proceso.getInputStream());
+            error = new InputStreamReader(proceso.getErrorStream());
+            stdInput = new BufferedReader(entrada);
+            stdError = new BufferedReader(error);
+            System.out.println("Comando a ejecutar " + cmd);
+            if ((linea = stdInput.readLine()) != null) {
+                salida = linea;
+                while ((linea = stdInput.readLine()) != null) {
+                    salida += linea + "\n";
+                }
+                System.out.println("Resultado del comando " + salida);
+            } else {
+                System.out.println("No se a producido ninguna salida");
+            }
+            if ((lineaError = stdError.readLine()) != null) {
+                salidaError = lineaError;
+                while ((lineaError = stdError.readLine()) != null) {
+                    salidaError += lineaError + "\n";
+                }
+                System.out.println("Resultado del error en comando " + salidaError);
+            }
+        } catch (IOException ioe) {
+            throw ioe;
+        } finally {
+            if (entrada != null) {
+                entrada.close();
+            }
+            if (error != null) {
+                error.close();
+            }
+            if (stdInput != null) {
+                stdInput.close();
+            }
+            if (stdError != null) {
+                stdError.close();
+            }
+        }
+        return salida;
+    }
+
+    public static String execute(String cmd) throws IOException {
+        String linea;
+        String salida = null;
+        String[] pipelineCmd = {
+            "/bin/bash",
+            "-c",
+            cmd
+        };
+        Process proceso = Runtime.getRuntime().exec(pipelineCmd);
+        BufferedReader stdInput;
+        try (InputStreamReader entrada = new InputStreamReader(proceso.getInputStream())) {
+            stdInput = new BufferedReader(entrada);
+            if ((linea = stdInput.readLine()) != null) {
+                salida = linea;
+                while ((linea = stdInput.readLine()) != null) {
+                    salida += linea + "\n";
+                }
+            }
+        }
+        stdInput.close();
+        return salida;
+    }
+
+    public static int executeExitValue(String cmd) throws IOException {
+        String[] pipelineCmd = {
+            "/bin/bash",
+            "-c",
+            cmd
+        };
+        Process proceso = Runtime.getRuntime().exec(pipelineCmd);
+        return proceso.exitValue();
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,73 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author benjamin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        
+        if(args.length != 1){
+            System.out.println("Ingrese las opciones necesarias. java -jar ConvertirDirectorioUTF8 ruta_directorio");
+            return;
+        }
+        String directorio = args[0];
+        File dir = new File(directorio);
+        List<String> archivos = getArchivosRecursivamente(dir, new ArrayList<String>());
+        for (String archivo : archivos) {
+            String cmdCodificacion = CommandService.execute("file -bi " + archivo);
+            String codec = cmdCodificacion.split("charset=")[1].trim();
+            System.out.println(archivo + " codec:" + codec);
+            imprimrArchivo(archivo, codec, "UTF-8");
+        }
+        System.out.println("archivos encontrados metodo return " + archivos.size());
+
+    }
+
+    public static void imprimrArchivo(String ruta, String codificacionEntrada, String codificacionSalida)
+            throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        BufferedWriter bw;
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(ruta), codificacionEntrada))) {
+            String sCadena;
+            String salida = "";
+            while ((sCadena = in.readLine()) != null) {
+                salida += sCadena + "\n";
+            }
+            System.out.println(salida);
+            File archivo = new File(ruta);
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write(salida);
+        }
+        bw.close();
+
+    }
+
+    public static List<String> getArchivosRecursivamente(File carpeta, List<String> archivos) {
+        for (File ficheroEntrada : carpeta.listFiles()) {
+            if (ficheroEntrada.isDirectory()) {
+                getArchivosRecursivamente(ficheroEntrada, archivos);
+            } else {
+                archivos.add(ficheroEntrada.getAbsolutePath());
+            }
+        }
+        return archivos;
+    }
+
+}
